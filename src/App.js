9b298047e5840e1f0f6e4c532a7fe2c768d72cea
jazz-code <ren.meta@gmail.com>
2019-08-05 15:49:47 -0400

    addItem function finished

--- src/App.js
+++ src/App.js
@@ -14,15 +14,24 @@ const list = [
   }
 ];
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
     this.state = {
       list
     };
   }
+
+  addItem = () => {
+    const newItem = {
+      name: itemName,
+      date: Date.now(),
+      completed: false
+    };
+    this.setState({
+      list: [...this.state.list, newItem]
+    });
+  };
+
   render() {
     return (
       <div>
